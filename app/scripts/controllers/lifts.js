268f88abcf504b0afc8f3d723893c2b7bd8d713a
Jason Divock <jdivock@gmail.com>
2014-02-17 16:59:08 -0500

    it almost looks respectable (not really)

--- app/scripts/controllers/lifts.js
+++ app/scripts/controllers/lifts.js
@@ -4,20 +4,59 @@ angular.module('lifterlyApp')
     .controller('LiftsCtrl', function($scope, Auth, User, Workout) {
         $scope.user = Auth.currentUser();
 
-        $scope.formLifts = [{}];
+        $scope.formLifts = [{
+            main: true
+        }];
+
+        console.log("user", $scope.user);
+
+        $scope.hasMain = function(liftArr) {
+            if (!liftArr) {
+                return false;
+            }
+            var hasMain = false;
+
+            for (var i = 0; i < liftArr.length; i++) {
+                if (liftArr[i].main) {
+                    hasMain = true;
+                }
+            }
+            return hasMain;
+        };
+
+        //TODO combine this with hasMain
+        $scope.hasAcc = function(liftArr) {
+            if (!liftArr) {
+                return false;
+            }
+            var hasAcc = false;
+
+            for (var i = 0; i < liftArr.length; i++) {
+                if (!liftArr[i].main) {
+                    hasAcc = true;
+                }
+            }
+            return hasAcc;
+        };
 
         $scope.saveLifts = function(form) {
-            console.log($scope.formLifts);
             Workout.save({
                 id: $scope.user._id,
                 lifts: $scope.formLifts
             }, function() {
+                // Load new lifts by reloading user profile
                 $scope.user = Auth.currentUser();
+
+                // Reset form
+                $scope.formLifts = [{}];
             });
         };
 
-        $scope.addLift = function() {
-            $scope.formLifts.push({});
+        $scope.addLift = function(isMain) {
+            $scope.formLifts.push({
+                main: isMain
+            });
+
         };
 
     });
