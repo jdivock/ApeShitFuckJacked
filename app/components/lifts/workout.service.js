af2f1161734732c94b78a37b9363e66b01311966
Jason Divock <jdivock@hugeinc.com>
2014-05-18 16:49:17 -0400

    Up and running with polymer

--- app/components/lifts/workout.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('lifterly.lifts')
-    .factory('Workout', function($resource) {
-        return $resource('/api/users/:id/workout', {
-            id: '@id'
-        });
-    });
--- app/components/profile/session.service.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('lifterly')
-    .factory('Session', function($resource) {
-        return $resource('/api/session/');
-    });
