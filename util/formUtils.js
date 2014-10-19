c4ed664901ca8042e6ee4f284e93a39e2fe9e839
Jason Divock <jdivock@hugeinc.com>
2014-10-19 10:30:04 -0400

    oops, forgot new util dir

---
+++ util/formUtils.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Date function added to format a date to work with html5 input type date
+ */
+Date.prototype.toDateInputValue = function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+};
+
+module.exports = {
+    validateEmail: function(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+};
