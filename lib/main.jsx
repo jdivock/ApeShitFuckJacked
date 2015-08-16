9325f372d4311c7d1eb6f2d62e6499e9a5ff0745
Jason Divock <jdivock@gmail.com>
2015-08-15 21:37:06 -0700

    yeah wahtevs

--- lib/main.jsx
+++ lib/main.jsx
@@ -5,6 +5,7 @@ var React = require('react'),
     Lifterly = require('components/Lifterly'),
     debug = require('debug');
 
+Parse.initialize("b6CKF1yyr40lUtp3o390OWxXQzKaK2SIFkNpmPUZ", "005j2FySLT2VC1tuWkendwiCqcw5msCKsXMwVQnm");
 
 debug.enable('*');
 
